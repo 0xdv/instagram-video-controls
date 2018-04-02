983509dbdfe7eeb037e03b2b74f29769d05ec538
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2018-04-02 14:21:30 +0300

    Keep volume value in localStorage

--- instagram-video-controls.user.js
+++ instagram-video-controls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Video Controls
 // @namespace    instagram_video
-// @version      0.1
+// @version      0.2
 // @description  Adds standart video controls for video in Instagram
 // @homepageURL  https://github.com/0xC0FFEEC0DE/instagram-video-controls
 // @supportURL   https://github.com/0xC0FFEEC0DE/instagram-video-controls/issues
@@ -19,12 +19,18 @@
     let videoObserver = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             let video = mutation.target;
+            //console.log(video);
             if(!video.controls) {
                 video.controls = 'controls';
                 let article = video.closest('article');
                 article.querySelectorAll('._7thjo, ._j12ff').forEach(trash => {
                     trash.parentNode.removeChild(trash);
                 });
+
+                video.volume = localStorage.getItem('video_volume') || 1;
+                video.onvolumechange = (event) => {
+                    localStorage.setItem('video_volume', event.target.volume);
+                };
             }
         });
     }).observe(document.body, {
