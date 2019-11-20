b2d615b2d68799cdf913e88b540657f9bb35b739
0xdv <0xc0ffec0de@gmail.com>
2019-11-20 19:28:52 +0200

    fixed error in console

--- instagram-video-controls.user.js
+++ instagram-video-controls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Video Controls
 // @namespace    instagram_video
-// @version      0.2.3
+// @version      0.2.4
 // @description  Adds standart video controls for video in Instagram
 // @homepageURL  https://github.com/0xC0FFEEC0DE/instagram-video-controls
 // @supportURL   https://github.com/0xC0FFEEC0DE/instagram-video-controls/issues
@@ -17,42 +17,41 @@
     'use strict';
     
     let videoObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            
-            // Check for the play button to be removed, i.e. the video is being played
-            if (mutation.removedNodes) {
-                mutation.removedNodes.forEach(removedNode => {
-                    if (removedNode.classList.contains('_8jZFn')) {
-                        
-                        // The <video/> element should be before the removed button
-                        var video = mutation.previousSibling;                      
-                        if (video && video.tagName && video.tagName.toLowerCase() == 'video') {
-                            
-                            if (!video.controls) {
-                                
-                                // Add native video controls
-                                video.controls = 'controls';
-                                
-                                // Remove overlay
-                                let article = video.closest('article');
-                                article.querySelectorAll('.PyenC, .fXIG0').forEach(trash => {
-                                    trash.parentNode.removeChild(trash);
-                                });
-                                
-                                // Keep volume value in localStorage
-                                video.volume = localStorage.getItem('video_volume') || 1;
-                                video.onvolumechange = (event) => {
-                                    localStorage.setItem('video_volume', event.target.volume);
-                                };
-                                
-                            }
-                            
-                        }
-                        
-                    }
-                });
+        mutations = Array.from(mutations);
+
+        function videoClassCount(removedNodes) {
+            return Array.from(removedNodes)
+                .filter(node => node.classList && node.classList.contains('_8jZFn')).length;
+        }
+
+        // Check for the play button to be removed, i.e. the video is being played
+        let videoMutations = mutations
+            .filter(m => m.removedNodes && videoClassCount(m.removedNodes) > 0);
+
+        if(videoMutations.length === 0) return;
+
+        videoMutations.forEach(m => {
+            // The <video/> element should be before the removed button
+            var video = m.previousSibling;
+            if (video && video.tagName && video.tagName.toLowerCase() == 'video') {
+                if (!video.controls) {
+
+                    // Add native video controls
+                    video.controls = 'controls';
+
+                    // Remove overlay
+                    let article = video.closest('article');
+                    article.querySelectorAll('.PyenC, .fXIG0').forEach(trash => {
+                        trash.parentNode.removeChild(trash);
+                    });
+                    
+                    // Keep volume value in localStorage
+                    video.volume = localStorage.getItem('video_volume') || 1;
+                    video.onvolumechange = (event) => {
+                        localStorage.setItem('video_volume', event.target.volume);
+                    };
+                }
             }
-            
         });
     }).observe(document.body, {
         childList: true,
